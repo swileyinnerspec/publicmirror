d72b13967362ef356444637e520e2af64d68de00
Your Name <you@example.com>
2022-12-29 11:47:07 -0500

    update on start

--- backgroundupdate.sh
+++ backgroundupdate.sh
@@ -1,4 +1,4 @@
-while inotifywait -m "$(git config --get remote.origin.url)"; do
+function update {
 	echo "update!"
 	git reset --hard
 	git pull
@@ -6,4 +6,8 @@ while inotifywait -m "$(git config --get remote.origin.url)"; do
 	echo "<pre>" >>./index.html
 	tree >>./index.html
 	git log --oneline >>./index.html
+}
+update
+while inotifywait -m "$(git config --get remote.origin.url)"; do
+	update
 done
