a7bf0685fa397a831ead54c68eb2ee9ad7c61e74
Your Name <you@example.com>
2022-12-29 13:52:03 -0500

    don't show index.html

--- backgroundupdate.sh
+++ backgroundupdate.sh
@@ -1,13 +1,15 @@
 function update {
 	git reset --hard
 	git pull
-	echo "<title>git repo</title><h1>Readme.html</h1>">./index.html
-	cp readme.html index.html
-	echo "<hr><h1>Reposotory data</h1><pre>" >>./index.html
-	tree >>./index.html
-	git branch >>./index.html
-	echo "</pre><hr><h2>Commits</h2><pre>" >>./index.html
-	git log --oneline >>./index.html
+	echo "<title>git repo</title><h1>Readme.html</h1>">./.index.html
+	cp readme.html .index.html
+	echo "<hr><h1>Reposotory data</h1><pre>" >>./.index.html
+	rm index.html
+	tree >>./.index.html
+	git branch >>./.index.html
+	echo "</pre><hr><h2>Commits</h2><pre>" >>./.index.html
+	git log --oneline >>./.index.html
+	mv .index.html index.html
 }
 update
 while inotifywait -m "$(git config --get remote.origin.url)"; do
