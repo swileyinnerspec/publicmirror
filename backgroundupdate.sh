a6502e359018f7851812ee69f790a6210a8b106a
Your Name <you@example.com>
2022-12-29 11:39:43 -0500

    change update to generate a nicer index

--- backgroundupdate.sh
+++ backgroundupdate.sh
@@ -1,4 +1,8 @@
 while inotifywait -m "$(git config --get remote.origin.url)"; do
 	echo "update!"
 	git pull
+	cp readme.html index.html
+	echo "<pre>" >>./index.html
+	tree >>./index.html
+	git log --oneline >>./index.html
 done
