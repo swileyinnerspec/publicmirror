604f4d6b5b90cfb5e86d18feaf9dd0a6e602135d
Your Name <you@example.com>
2022-12-29 11:42:51 -0500

    fix update helper

--- backgroundupdate.sh
+++ backgroundupdate.sh
@@ -2,7 +2,7 @@ while inotifywait -m "$(git config --get remote.origin.url)"; do
 	echo "update!"
 	git reset --hard
 	git pull
-	cp readme.html index.html
+	cp readme index.html
 	echo "<pre>" >>./index.html
 	tree >>./index.html
 	git log --oneline >>./index.html
