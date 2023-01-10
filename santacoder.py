de4f613aa3eb8a8e902524221fe964d33309266e
Stephen Wiley <swiley@swiley.net>
2023-01-10 13:03:44 +0000

    fix overwritting flan model with santacoder

--- santacoder.py
+++ santacoder.py
@@ -2,15 +2,15 @@
 import sys
 import os
 import socket
-path = "~/santa.sock"
-os.remove(path)
+path = "/var/run/santa.sock"
 checkpoint = "bigcode/santacoder"
 device = "cpu" # for GPU usage or "cpu" for CPU usage
 max_tokens=100
 def serve(gen,path):
+    os.remove(path)
     sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     sock.bind(path)
-    os.chmod(path, 0777)
+    os.chmod(path, "0777")
     sock.listen(1)
     print('listening on',path)
     while True:
@@ -29,12 +29,12 @@ if len(sys.argv) == 2 and sys.argv[1] == "serve":
     print("loading flan-t5...");
     from transformers import T5Tokenizer, T5ForConditionalGeneration
 
-    tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-xl")
-    model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-xl").to("cpu")
+    flantokenizer = T5Tokenizer.from_pretrained("google/flan-t5-xl")
+    flanmodel = T5ForConditionalGeneration.from_pretrained("google/flan-t5-xl").to("cpu")
     def flangen(input_text):
         if(len(input_text)>1):
-            input_ids = tokenizer(input_text, return_tensors="pt").input_ids.to("cpu")
-            output = model.generate(input_ids, max_length=100)
+            input_ids = flantokenizer(input_text, return_tensors="pt").input_ids.to("cpu")
+            output = flanmodel.generate(input_ids, max_length=100)
             return tokenizer.decode(output[0], skip_special_tokens=True)
         else: return "no query"
     print("loading santa model...")
