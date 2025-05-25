a02dd94f1c7c2deb6ed41094ac5a7bb1b0803b2e
Stephen Wiley <swiley@swiley.net>
2025-05-25 12:32:15 -0400

    use fvwm css

--- ci.sh
+++ ci.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-cp -r ./swiley.net/* /var/www/html/swiley.net/ 
+mkdir -p /var/www/html/swiley.net/
+mkdir -p /var/www/html/dopes.top/
+mkdir -p /var/www/html/bingestop.com/
+cp -r ./swiley.net/* /var/www/html/swiley.net/
+cp -r ./dopes.top/* /var/www/html/dopes.top/
+cp -r ./dopes.top/* /var/www/html/bingestop.com/
